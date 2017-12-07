8ddfef47edd2155ad83e7d98435596b18ba9bed2
Harry Grumbar <harry@goodlord.co>
2017-12-07 14:13:46 +0000

    Add option utils and more tests

--- src/option.spec.ts
+++ src/option.spec.ts
@@ -1,80 +1,107 @@
-import { Option, Some, None } from './option'
+import { Option, Some, None, isOption } from './option'
 
-describe('Option - None', () => {
-  it('gets a string representation of itself', () => {
-    expect(None().toString()).toBe('None')
-  })
+describe('Option', () => {
   it('constructs a None from a null or undefined value', () => {
     const option = Option(null)
 
     expect(option.type).toBe('none')
   })
 
-  it('has a map method that does nothing', () => {
-    const fn = jest.fn()
-    const option = None().map(fn)
-
-    expect(option.type).toBe('none')
-    expect(fn.mock.calls.length).toBe(0)
-  })
-
-  it('has a flatMap method', () => {
-    const result = None().flatMap(val => `${val}bar`)
-
-    expect(result.type).toBe('none')
-  })
-
-  it('implements a fold method', () => {
-    const option = None()
-
-    expect(option.fold(() => 'foo', v => v)).toBe('foo')
-  })
-
-  it('implements a getOrElse method', () => {
-    const option = None()
-
-    expect(option.getOrElse('foo')).toBe('foo')
-  })
-})
-
-describe('Option - Some', () => {
-  it('gets a string representation of itself', () => {
-    expect(Some('foo').toString()).toBe('Some(foo)')
-  })
-
   it('constructs a Some from a value', () => {
     const option = Option('hello, world')
-
+    expect(option.type).toBe('some')
     expect(option.get()).toBe('hello, world')
   })
 
-  it('has a map method that operates on the value', () => {
-    const option = Option('hello').map(s => s.length)
+  describe('None', () => {
+    it('gets a string representation of itself', () => {
+      expect(`${None()}`).toBe('None')
+    })
 
-    expect(option.get()).toBe(5)
+    it('asserts it is not a some', () => {
+      expect(Some('foo').isSome()).toBe(true)
+    })
+
+    it('implements a map method that does nothing', () => {
+      const fn = jest.fn()
+      const option = None().map(fn)
+
+      expect(option.type).toBe('none')
+      expect(fn.mock.calls.length).toBe(0)
+    })
+
+    it('implements a flatMap method', () => {
+      const result = None().flatMap(val => `${val}bar`)
+
+      expect(result.type).toBe('none')
+    })
+
+    it('implements a fold method', () => {
+      const option = None()
+
+      expect(option.fold(() => 'foo', v => v)).toBe('foo')
+    })
+
+    it('implements a getOrElse method', () => {
+      const option = None()
+
+      expect(option.getOrElse('foo')).toBe('foo')
+    })
   })
 
-  it('has a flatMap method', () => {
-    const result = Some('foo').flatMap(val => `${val}bar`)
+  describe('Some', () => {
+    it('gets a string representation of itself', () => {
+      expect(`${Some('foo')}`).toBe('Some(foo)')
+    })
 
-    expect(result).toBe('foobar')
+    it('asserts it is a some', () => {
+      expect(Some('foo').isSome()).toBe(true)
+    })
+
+    it('has a map method that operates on the value', () => {
+      const option = Some('hello').map(s => s.length)
+
+      expect(option.get()).toBe(5)
+    })
+
+    it('implements a flatMap method', () => {
+      const result = Some('foo').flatMap(val => `${val}bar`)
+
+      expect(result).toBe('foobar')
+    })
+
+    it('returns a None if the map method results in undefined or null', () => {
+      const option = Option('hello').map(() => null)
+
+      expect(option.type).toBe('none')
+    })
+
+    it('implements a fold method', () => {
+      const option = Some('hello')
+
+      expect(option.fold(() => 'foo', v => v)).toBe('hello')
+    })
+
+    it('implements a filter method', () => {
+      const option = Some('hello')
+
+      expect(option.filter(v => v.length > 10).type).toBe('none')
+      expect(option.filter(v => v.length < 10).type).toBe('some')
+    })
+
+    it('implements a getOrElse method', () => {
+      const option = Some('hello')
+
+      expect(option.getOrElse('foo')).toBe('hello')
+    })
   })
 
-  it('returns a None if the map method results in undefined or null', () => {
-    const option = Option('hello').map(() => null)
-
-    expect(option.type).toBe('none')
-  })
-
-  it('implements a fold method', () => {
-    const option = Some('hello')
-
-    expect(option.fold(() => 'foo', v => v)).toBe('hello')
-  })
-
-  it('implements a getOrElse method', () => {
-    const option = Some('hello')
-
-    expect(option.getOrElse('foo')).toBe('hello')
+  describe('utils', () => {
+    it('asserts if a value is an Option', () => {
+      expect(isOption(None())).toBe(true)
+      expect(isOption(Some('foo'))).toBe(true)
+      expect(isOption('foo')).toBe(false)
+      expect(isOption({ foo: 'bar' })).toBe(false)
+    })
   })
 })
--- src/option.ts
+++ src/option.ts
@@ -1,20 +1,17 @@
 export interface Option<T> {
   type: 'some' | 'none'
+  isSome: () => boolean
   get: () => T | void
   map<K>(fn: (value: T) => K): Option<K>
   flatMap<K>(fn: (value: T) => K | None)
   fold<K, P>(leftFn: () => K, rightFn: (value: T) => P): K | P
+  filter(fn: (value: T) => boolean): Some<T> | None
   getOrElse<K>(alternative: K): T | K
   toString: () => string
 }
 
 export function Option<T>(value: T): Option<T> {
   return value === null || value === undefined ? None() : Some(value)
-}
-
-export interface None extends Option<never> {
-  type: 'none'
-  get: () => void
 }
 
 export interface Some<T> extends Option<T> {
@@ -25,23 +22,36 @@ export interface Some<T> extends Option<T> {
 export function Some<T>(value: T): Some<T> {
   return {
     type: 'some',
+    isSome: () => true,
     get: () => value,
     map: fn => Option(fn(value)),
     flatMap: fn => fn(value),
     fold: (_, rightFn) => rightFn(value),
+    filter: fn => (fn(value) ? Some(value) : None()),
     getOrElse: () => value,
     toString: () => `Some(${value})`,
   }
 }
 
+export interface None extends Option<never> {
+  type: 'none'
+  get: () => void
+}
+
 export function None(): None {
   return {
     type: 'none',
+    isSome: () => false,
     get: () => {},
     map: None,
     flatMap: None,
     fold: leftFn => leftFn(),
+    filter: None,
     getOrElse: alternative => alternative,
     toString: () => 'None',
   }
+}
+
+export function isOption<T>(value: any): value is Option<T> {
+  return value && (value.type === 'some' || value.type === 'none')
 }
