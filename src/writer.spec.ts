3a639c3a6f014bf05d5e26773cfe27c78cef70d4
Harry Grumbar <harry@goodlord.co>
2017-12-14 13:50:25 +0000

    Add map, mapWritten, bimap and mapBoth methods

--- src/writer.spec.ts
+++ src/writer.spec.ts
@@ -8,12 +8,62 @@ describe('Writer', () => {
     expect(writer.written().head()).toBe('a message')
   })
 
-  it('maps 2 writers together, preserving their logs', () => {
+  it('flat maps 2 writers together, preserving their logs', () => {
     const writer = Writer(List('z'), 30).flatMap(a => {
       return Writer(List('a', 'b'), 12 + a)
     })
 
     expect(writer.written().toArray()).toEqual(['z', 'a', 'b'])
     expect(writer.value()).toBe(42)
+  })
+
+  it('flat maps 2 writers with an string log', () => {
+    const writer = Writer('first', 'foo').flatMap(word => {
+      return Writer('second', word.toUpperCase())
+    })
+
+    expect(writer.run()).toEqual(['firstsecond', 'FOO'])
+  })
+
+  it('maps over a writer', () => {
+    const writer = Writer('first', 5).map(n => n + 5)
+
+    expect(writer.run()).toEqual(['first', 10])
+  })
+
+  it('maps over the written part of the writer to something of the same type', () => {
+    const writer = Writer('something', 'foo').mapWritten(log => {
+      return log.toUpperCase()
+    })
+
+    expect(writer.written()).toEqual('SOMETHING')
+  })
+
+  it('maps over the written part of the writer to something of a different type', () => {
+    const writer = Writer('something', 'foo').mapWritten(log => {
+      return List(log)
+    })
+
+    expect(writer.written().toArray()).toEqual(['something'])
+  })
+
+  it('bimaps over a writer', () => {
+    const writer = Writer(List('a', 'b'), 27).bimap(
+      log => log.map(l => l.toUpperCase()).toArray(),
+      res => res * 100,
+    )
+
+    expect(writer.written()).toEqual(['A', 'B'])
+    expect(writer.value()).toBe(2700)
+  })
+
+  it('maps over both params', () => {
+    const writer = Writer(List('a', 'b'), 27).mapBoth((log, res) => [
+      log.map(l => l.toUpperCase()).toArray(),
+      res * 100,
+    ])
+
+    expect(writer.written()).toEqual(['A', 'B'])
+    expect(writer.value()).toBe(2700)
   })
 })
--- src/writer.ts
+++ src/writer.ts
@@ -1,22 +1,40 @@
-import { List } from './list'
-
-export interface Writer<W, T> {
-  run: () => [List<W>, T]
-  value: () => T
-  written: () => List<W>
-  inspect: () => string
-  flatMap: <K>(fn: (val: T) => Writer<W, K>) => Writer<W, K>
+export interface SemiGroup<T> {
+  concat: (a: T) => T
 }
 
-export function Writer<W, T>(log: List<W>, val: T): Writer<W, T> {
+export interface Monoid<T> extends SemiGroup<T> {
+  empty: () => T
+}
+
+export interface Writer<W extends SemiGroup<W>, T> {
+  run: () => [W, T]
+  value: () => T
+  written: () => W
+  inspect: () => string
+  flatMap: <K>(fn: (val: T) => Writer<W, K>) => Writer<W, K>
+  map: <K>(fn: (val: T) => K) => Writer<W, K>
+  // bit of a hack as TS can't seem to infer the returned log type
+  mapWritten: <L extends SemiGroup<any>>(fn: (log: W) => L) => Writer<L, T>
+  bimap: <L extends SemiGroup<any>, K>(logFn: (log: W) => L, valFn: (val: T) => K) => Writer<L, K>
+  mapBoth: <L extends SemiGroup<any>, K>(fn: (log: W, val: T) => [L, K]) => Writer<L, K>
+}
+
+export function Writer<W extends SemiGroup<W>, T>(log: W, val: T): Writer<W, T> {
   return {
     inspect: () => `Writer(${log},${val})`,
     run: () => [log, val],
     value: () => val,
     written: () => log,
     flatMap: fn => {
-      const newWriter = fn(val)
-      return Writer(log.concat(newWriter.written()), newWriter.value())
+      const w = fn(val)
+      return Writer(log.concat(w.written()), w.value())
+    },
+    map: fn => Writer(log, fn(val)),
+    mapWritten: fn => Writer(fn(log), val),
+    bimap: (logFn, valFn) => Writer(logFn(log), valFn(val)),
+    mapBoth: fn => {
+      const [l, v] = fn(log, val)
+      return Writer(l, v)
     },
   }
 }
