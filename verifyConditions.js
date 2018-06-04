54a64a4ef9db181c05a4b2e086e90bc0d23bf251
harrygr <harry@bulb.co.uk>
2018-06-04 17:00:15 +0100

    feat: Update semantic release

--- verifyConditions.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(pluginConfig, config, callback) {
-  if (config.env.CI === 'true') {
-    callback(null)
-  } else {
-    callback(new Error("Not running on CI, won't be published."))
-  }
-}
